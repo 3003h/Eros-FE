27e9ecc6a742b6f731bbc959ed97e1ce70a60092
honjow <honjow311@gmail.com>
2020-07-09 00:33:51 +0800

    tag组件调整

---
+++ lib/models/galleryComment.g.dart
@@ -0,0 +1,23 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'galleryComment.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+GalleryComment _$GalleryCommentFromJson(Map<String, dynamic> json) {
+  return GalleryComment()
+    ..name = json['name'] as String
+    ..time = json['time'] as String
+    ..context = json['context'] as String
+    ..score = json['score'] as String;
+}
+
+Map<String, dynamic> _$GalleryCommentToJson(GalleryComment instance) =>
+    <String, dynamic>{
+      'name': instance.name,
+      'time': instance.time,
+      'context': instance.context,
+      'score': instance.score
+    };
