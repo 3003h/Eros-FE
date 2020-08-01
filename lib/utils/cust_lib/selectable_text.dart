b3be48d9ec9372611dc4778725b4f37b2a543be3
honjow <honjow311@gmail.com>
2020-08-01 22:02:38 +0800

    改用 stable 包

--- lib/utils/cust_lib/selectable_text.dart
+++ lib/utils/cust_lib/selectable_text.dart
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// @dart = 2.8
-
 import 'dart:ui';
 
 import 'package:flutter/cupertino.dart';
@@ -32,7 +30,7 @@ class _TextSpanEditingController extends TextEditingController {
 
   @override
   TextSpan buildTextSpan({TextStyle style, bool withComposing}) {
-    // This does not care about composing.
+    // TODO(chunhtai): Implement composing.
     return TextSpan(
       style: style,
       children: <TextSpan>[_textSpan],
@@ -41,8 +39,7 @@ class _TextSpanEditingController extends TextEditingController {
 
   @override
   set text(String newText) {
-    // This should never be reached.
-    throw UnimplementedError();
+    // TODO(chunhtai): Implement value editing.
   }
 }
 
@@ -216,14 +213,12 @@ class SelectableText extends StatefulWidget {
     this.minLines,
     this.maxLines,
     this.cursorWidth = 2.0,
-    this.cursorHeight,
     this.cursorRadius,
     this.cursorColor,
     this.dragStartBehavior = DragStartBehavior.start,
     this.enableInteractiveSelection = true,
     this.onTap,
     this.scrollPhysics,
-    this.textHeightBehavior,
     this.textWidthBasis,
   })  : assert(showCursor != null),
         assert(autofocus != null),
@@ -267,14 +262,12 @@ class SelectableText extends StatefulWidget {
     this.minLines,
     this.maxLines,
     this.cursorWidth = 2.0,
-    this.cursorHeight,
     this.cursorRadius,
     this.cursorColor,
     this.dragStartBehavior = DragStartBehavior.start,
     this.enableInteractiveSelection = true,
     this.onTap,
     this.scrollPhysics,
-    this.textHeightBehavior,
     this.textWidthBasis,
   })  : assert(showCursor != null),
         assert(autofocus != null),
@@ -365,9 +358,6 @@ class SelectableText extends StatefulWidget {
   /// {@macro flutter.widgets.editableText.cursorWidth}
   final double cursorWidth;
 
-  /// {@macro flutter.widgets.editableText.cursorHeight}
-  final double cursorHeight;
-
   /// {@macro flutter.widgets.editableText.cursorRadius}
   final Radius cursorRadius;
 
@@ -389,8 +379,7 @@ class SelectableText extends StatefulWidget {
   /// If not set, select all and copy will be enabled by default.
   final ToolbarOptions toolbarOptions;
 
-  /// True if interactive selection is enabled based on the values of
-  /// [enableInteractiveSelection].
+  /// {@macro flutter.rendering.editable.selectionEnabled}
   bool get selectionEnabled {
     return enableInteractiveSelection;
   }
@@ -414,9 +403,6 @@ class SelectableText extends StatefulWidget {
 
   /// {@macro flutter.widgets.editableText.scrollPhysics}
   final ScrollPhysics scrollPhysics;
-
-  /// {@macro flutter.dart:ui.textHeightBehavior}
-  final TextHeightBehavior textHeightBehavior;
 
   /// {@macro flutter.painting.textPainter.textWidthBasis}
   final TextWidthBasis textWidthBasis;
@@ -447,8 +433,6 @@ class SelectableText extends StatefulWidget {
         DoubleProperty('textScaleFactor', textScaleFactor, defaultValue: null));
     properties
         .add(DoubleProperty('cursorWidth', cursorWidth, defaultValue: 2.0));
-    properties
-        .add(DoubleProperty('cursorHeight', cursorHeight, defaultValue: null));
     properties.add(DiagnosticsProperty<Radius>('cursorRadius', cursorRadius,
         defaultValue: null));
     properties.add(DiagnosticsProperty<Color>('cursorColor', cursorColor,
@@ -459,9 +443,6 @@ class SelectableText extends StatefulWidget {
         ifFalse: 'selection disabled'));
     properties.add(DiagnosticsProperty<ScrollPhysics>(
         'scrollPhysics', scrollPhysics,
-        defaultValue: null));
-    properties.add(DiagnosticsProperty<TextHeightBehavior>(
-        'textHeightBehavior', textHeightBehavior,
         defaultValue: null));
   }
 }
@@ -501,7 +482,6 @@ class _SelectableTextState extends State<SelectableText>
         _SelectableTextSelectionGestureDetectorBuilder(state: this);
     _controller = _TextSpanEditingController(
         textSpan: widget.textSpan ?? TextSpan(text: widget.data));
-    _controller.addListener(_onControllerChanged);
   }
 
   @override
@@ -509,34 +489,18 @@ class _SelectableTextState extends State<SelectableText>
     super.didUpdateWidget(oldWidget);
     if (widget.data != oldWidget.data ||
         widget.textSpan != oldWidget.textSpan) {
-      _controller.removeListener(_onControllerChanged);
       _controller = _TextSpanEditingController(
           textSpan: widget.textSpan ?? TextSpan(text: widget.data));
-      _controller.addListener(_onControllerChanged);
     }
     if (_effectiveFocusNode.hasFocus && _controller.selection.isCollapsed) {
       _showSelectionHandles = false;
-    } else {
-      _showSelectionHandles = true;
     }
   }
 
   @override
   void dispose() {
     _focusNode?.dispose();
-    _controller.removeListener(_onControllerChanged);
     super.dispose();
-  }
-
-  void _onControllerChanged() {
-    final bool showSelectionHandles =
-        !_effectiveFocusNode.hasFocus || !_controller.selection.isCollapsed;
-    if (showSelectionHandles == _showSelectionHandles) {
-      return;
-    }
-    setState(() {
-      _showSelectionHandles = showSelectionHandles;
-    });
   }
 
   void _handleSelectionChanged(
@@ -656,8 +620,6 @@ class _SelectableTextState extends State<SelectableText>
         readOnly: true,
         textWidthBasis:
             widget.textWidthBasis ?? defaultTextStyle.textWidthBasis,
-        textHeightBehavior:
-            widget.textHeightBehavior ?? defaultTextStyle.textHeightBehavior,
         showSelectionHandles: _showSelectionHandles,
         showCursor: widget.showCursor,
         controller: _controller,
@@ -679,7 +641,6 @@ class _SelectableTextState extends State<SelectableText>
         onSelectionHandleTapped: _handleSelectionHandleTapped,
         rendererIgnoresPointer: true,
         cursorWidth: widget.cursorWidth,
-//        cursorHeight: widget.cursorHeight,
         cursorRadius: cursorRadius,
         cursorColor: cursorColor,
         cursorOpacityAnimates: cursorOpacityAnimates,
@@ -689,7 +650,6 @@ class _SelectableTextState extends State<SelectableText>
         enableInteractiveSelection: widget.enableInteractiveSelection,
         dragStartBehavior: widget.dragStartBehavior,
         scrollPhysics: widget.scrollPhysics,
-        //
         selectionHeightStyle: BoxHeightStyle.max,
       ),
     );
