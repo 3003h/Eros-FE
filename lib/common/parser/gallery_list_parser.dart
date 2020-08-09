512a5237a0896d6be8eb3cf54bce969c3bdb1b2b
honjow <honjow311@gmail.com>
2020-08-09 22:43:38 +0800

    点击url可打开对应画廊

--- lib/common/parser/gallery_list_parser.dart
+++ lib/common/parser/gallery_list_parser.dart
@@ -1,44 +1,32 @@
-import 'dart:convert';
-
 import 'package:FEhViewer/common/tag_database.dart';
 import 'package:FEhViewer/models/index.dart';
-import 'package:FEhViewer/utils/dio_util.dart';
 import 'package:FEhViewer/utils/utility.dart';
 import 'package:FEhViewer/values/const.dart';
 import 'package:html/dom.dart' as dom;
 import 'package:html/parser.dart' show parse;
-import 'package:html_unescape/html_unescape.dart';
 import 'package:intl/intl.dart';
 import 'package:tuple/tuple.dart';
 
 class GalleryListParser {
-  /// 获取api
-  static Future getGalleryApi(String req) async {
-    HttpManager httpManager = HttpManager.getInstance(EHConst.EH_BASE_URL);
-    const url = "/api.php";
-
-    var response = await httpManager.postForm(url, data: req);
-
-    return response;
-  }
-
   /// 列表数据处理
   static Future<Tuple2<List<GalleryItem>, int>> parseGalleryList(
       String response,
-      {isFavorite = false}) async {
-    var document = parse(response);
+      {bool isFavorite = false}) async {
+    final dom.Document document = parse(response);
 
-    final _gallerySelector =
-        "body > div.ido > div:nth-child(2) > table > tbody > tr";
-    final _favSelector = "body > div.ido > form > table.itg.gltc > tbody > tr";
+    const String _gallerySelector =
+        'body > div.ido > div:nth-child(2) > table > tbody > tr';
+    const String _favSelector =
+        'body > div.ido > form > table.itg.gltc > tbody > tr';
 
-    final _listSelector = isFavorite ? _favSelector : _gallerySelector;
+    final String _listSelector = isFavorite ? _favSelector : _gallerySelector;
 
-    final _galleryPageSelector =
+    const String _galleryPageSelector =
         'body > div.ido > div:nth-child(2) > table.ptt > tbody > tr > td';
-    final _favPageSelector =
+    const String _favPageSelector =
         'body > div.ido > form > table.ptt > tbody > tr > td';
-    final _pageSelector = isFavorite ? _favPageSelector : _galleryPageSelector;
+    final String _pageSelector =
+        isFavorite ? _favPageSelector : _galleryPageSelector;
 
     // final fav =
     //     document.querySelector("body > div.ido > form > p")?.text?.trim() ?? "";
@@ -47,7 +35,7 @@ class GalleryListParser {
     int _maxPage = 0;
     List<dom.Element> _pages = document.querySelectorAll(_pageSelector);
     if (_pages.length > 2) {
-      dom.Element _maxPageElem = _pages[_pages.length - 2];
+      final dom.Element _maxPageElem = _pages[_pages.length - 2];
       _maxPage = int.parse(_maxPageElem.text.trim());
 //      Global.logger.v('_maxPage $_maxPage');
     }
@@ -56,40 +44,41 @@ class GalleryListParser {
     List<dom.Element> gallerys = document.querySelectorAll(_listSelector);
 //    Global.logger.v('gallerys ${gallerys.length}');
 
-    List<GalleryItem> _gallaryItems = [];
-    for (var tr in gallerys) {
-      final category = tr.querySelector('td.gl1c.glcat > div')?.text?.trim();
+    final List<GalleryItem> _gallaryItems = [];
+    for (final dom.Element tr in gallerys) {
+      final String category =
+          tr.querySelector('td.gl1c.glcat > div')?.text?.trim();
 
       // 表头或者广告
       if (category == null || category.isEmpty) {
         continue;
       }
 
-      final title =
+      final String title =
           tr.querySelector('td.gl3c.glname > a > div.glink')?.text?.trim();
 
-      final url =
+      final String url =
           tr.querySelector('td.gl3c.glname > a')?.attributes['href'] ?? '';
 
-      RegExp urlRex = new RegExp(r"/g/(\d+)/(\w+)/$");
-      var urlRult = urlRex.firstMatch(url);
+      final RegExp urlRex = RegExp(r'/g/(\d+)/(\w+)/$');
+      final RegExpMatch urlRult = urlRex.firstMatch(url);
 
-      final gid = urlRult.group(1);
-      final token = urlRult.group(2);
+      final String gid = urlRult.group(1);
+      final String token = urlRult.group(2);
 
       // tags
-      List tags = tr.querySelectorAll('div.gt');
+      final List tags = tr.querySelectorAll('div.gt');
 
       // 英文tag
       final List<String> simpleTags = [];
-      for (var tag in tags) {
-        var tagText = tag.text.trim();
+      for (final tag in tags) {
+        final tagText = tag.text.trim();
         simpleTags.add(tagText);
       }
 
       // 中文tag
       final List<String> simpleTagsTranslate = [];
-      for (var tag in tags) {
+      for (final tag in tags) {
         final tagText = tag.text.trim();
         simpleTagsTranslate
             .add(await EhTagDatabase.getTranTag(tagText) ?? tagText);
@@ -102,45 +91,45 @@ class GalleryListParser {
       final String imgUrl = imgDataSrc ?? imgSrc ?? '';
 
       // 图片宽高
-      final imageStyle = img.attributes['style'];
-      var match =
+      final String imageStyle = img.attributes['style'];
+      final RegExpMatch match =
           RegExp(r'height:(\d+)px;width:(\d+)px').firstMatch(imageStyle);
-      final imageHeight = double.parse(match[1]);
-      final imageWidth = double.parse(match[2]);
+      final double imageHeight = double.parse(match[1]);
+      final double imageWidth = double.parse(match[2]);
 
       // 评分星级计算 (api获取不到评分时用)
-      final ratPx = tr
+      final String ratPx = tr
           .querySelector('td.gl2c > div:nth-child(2) > div.ir')
           .attributes['style'];
-      RegExp pxA = new RegExp(r"-?(\d+)px\s+-?(\d+)px");
-      var px = pxA.firstMatch(ratPx);
+      final RegExp pxA = RegExp(r'-?(\d+)px\s+-?(\d+)px');
+      final RegExpMatch px = pxA.firstMatch(ratPx);
 
       //
-      final ratingFB = (80.0 - double.parse(px.group(1))) / 16.0 -
+      final double ratingFB = (80.0 - double.parse(px.group(1))) / 16.0 -
           (px.group(2) == '21' ? 0.5 : 0.0);
 
 //      Global.loggerNoStack.i('ratingFB $ratingFB');
 
-      final postTime =
+      final String postTime =
           tr.querySelector('td.gl2c > div:nth-child(2) > div')?.text?.trim() ??
               '';
       DateTime time =
           DateFormat('yyyy-MM-dd HH:mm').parseUtc(postTime).toLocal();
 
-      final postTimeLocal = DateFormat('yyyy-MM-dd HH:mm').format(time);
+      final String postTimeLocal = DateFormat('yyyy-MM-dd HH:mm').format(time);
 
       // 收藏标志
-      final favTitle = tr
+      final String favTitle = tr
               .querySelector('td.gl2c > div:nth-child(2) > div')
               ?.attributes['title'] ??
           '';
 
-      var favcat = '';
+      String favcat = '';
       if (favTitle.isNotEmpty) {
-        var favcatStyle = tr
+        final String favcatStyle = tr
             .querySelector('td.gl2c > div:nth-child(2) > div')
             ?.attributes['style'];
-        var favcatColor = RegExp(r'border-color:(#\w{3});')
+        final String favcatColor = RegExp(r'border-color:(#\w{3});')
                 ?.firstMatch(favcatStyle)
                 ?.group(1) ??
             '';
@@ -165,66 +154,10 @@ class GalleryListParser {
     }
 
     // 通过api请求获取更多信息
-    if (_gallaryItems.length > 0) {
-      await getMoreGalleryInfo(_gallaryItems);
+    if (_gallaryItems.isNotEmpty) {
+      await Api.getMoreGalleryInfo(_gallaryItems);
     }
 
     return Tuple2(_gallaryItems, _maxPage);
-  }
-
-  /// 通过api请求获取更多信息
-  /// 例如
-  /// 画廊评分
-  /// 日语标题
-  /// 等等
-  static Future<List<GalleryItem>> getMoreGalleryInfo(
-      List<GalleryItem> galleryItems) async {
-    // Global.logger.i('api qry items ${galleryItems.length}');
-    if (galleryItems.length == 0) {
-      return galleryItems;
-    }
-
-    // 通过api获取画廊详细信息
-    List _gidlist = [];
-
-    galleryItems.forEach((galleryItem) {
-      _gidlist.add([galleryItem.gid, galleryItem.token]);
-    });
-
-    // 25个一组分割
-    List _group = EHUtils.splitList(_gidlist, 25);
-
-    List rultList = [];
-
-    // 查询 合并结果
-    for (int i = 0; i < _group.length; i++) {
-      Map reqMap = {'gidlist': _group[i], 'method': 'gdata'};
-      String reqJsonStr = jsonEncode(reqMap);
-      var rult = await getGalleryApi(reqJsonStr);
-
-      var jsonObj = jsonDecode(rult.toString());
-      var tempList = jsonObj['gmetadata'];
-      rultList.addAll(tempList);
-    }
-
-    var unescape = new HtmlUnescape();
-
-    for (int i = 0; i < galleryItems.length; i++) {
-      galleryItems[i].englishTitle = unescape.convert(rultList[i]['title']);
-      galleryItems[i].japaneseTitle =
-          unescape.convert(rultList[i]['title_jpn']);
-
-      var rating = rultList[i]['rating'];
-      // Global.loggerNoStack.v('$rating');
-      galleryItems[i].rating = rating != null
-          ? double.parse(rating)
-          : galleryItems[i].ratingFallBack;
-
-//      galleryItems[i].imgUrl = rultList[i]['thumb'];
-      galleryItems[i].filecount = rultList[i]['filecount'];
-      galleryItems[i].uploader = rultList[i]['uploader'];
-    }
-
-    return galleryItems;
   }
 }
