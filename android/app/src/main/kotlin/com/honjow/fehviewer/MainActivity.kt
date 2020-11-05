2b45492e77298d7570acf1b2f45499b909e20ee2
honjow <honjow311@gmail.com>
2020-11-05 19:54:27 +0800

    add Share Extension

--- android/app/src/main/kotlin/com/honjow/fehviewer/MainActivity.kt
+++ android/app/src/main/kotlin/com/honjow/fehviewer/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.honjow.fehviewer
 
+import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
+import android.content.Intent.FLAG_ACTIVITY_NEW_TASK
 
 class MainActivity: FlutterActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
