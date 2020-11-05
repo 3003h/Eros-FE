bcf5c35e8161e70459bb99f3da42f77467b32c53
honjow <honjow311@gmail.com>
2020-11-05 17:16:02 +0800

    add Share Extension

--- android/app/src/main/kotlin/com/honjow/fehviewer/MainActivity.kt
+++ android/app/src/main/kotlin/com/honjow/fehviewer/MainActivity.kt
@@ -3,4 +3,13 @@ package com.honjow.fehviewer
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        if (intent.getIntExtra("org.chromium.chrome.extra.TASK_ID", -1) == this.taskId) {
+            this.finish()
+            intent.addFlags(FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+        }
+        super.onCreate(savedInstanceState)
+    }
 }
