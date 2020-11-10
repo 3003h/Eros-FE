8a043b41d30c8d8b24976558e1e47e71d39cd9d2
honjow <honjow311@gmail.com>
2020-11-10 19:57:58 +0800

    简单搜索功能

--- lib/pages/tab/search_page.dart
+++ lib/pages/tab/search_page.dart
@@ -1,10 +1,13 @@
 import 'package:FEhViewer/common/global.dart';
 import 'package:FEhViewer/generated/l10n.dart';
+import 'package:FEhViewer/models/states/ehconfig_model.dart';
 import 'package:FEhViewer/pages/tab/gallery_base.dart';
+import 'package:FEhViewer/route/navigator_util.dart';
 import 'package:FEhViewer/values/theme_colors.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:provider/provider.dart';
 
 class GallerySearchPage extends StatefulWidget {
   @override
@@ -12,28 +15,42 @@ class GallerySearchPage extends StatefulWidget {
 }
 
 class _GallerySearchPageState extends State<GallerySearchPage> {
+  // 搜索内容的控制器
+  final TextEditingController _searchTextController = TextEditingController();
+
+  void _jumpSearch() {
+    final String _searchText = _searchTextController.text;
+    final int _catNum =
+        Provider.of<EhConfigModel>(context, listen: false).catFilter;
+    if (_searchText.isNotEmpty) {
+      NavigatorUtil.goGalleryList(
+        context,
+        simpleSearch: _searchText,
+        cats: _catNum,
+        replace: true,
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     final S ln = S.of(context);
 
-    Widget cfp = CupertinoPageScaffold(
+    final Widget cfp = CupertinoPageScaffold(
       resizeToAvoidBottomInset: false,
       navigationBar: CupertinoNavigationBar(
-        padding: EdgeInsetsDirectional.only(start: 0),
+        padding: const EdgeInsetsDirectional.only(start: 0),
 //        border: null,
         backgroundColor: ThemeColors.navigationBarBackground,
-        middle: CupertinoTextField(),
+        middle: CupertinoTextField(
+          controller: _searchTextController,
+          onEditingComplete: () {
+            // 点击键盘完成
+            _jumpSearch();
+          },
+        ),
         transitionBetweenRoutes: false,
         previousPageTitle: '返回',
-//        leading: CupertinoButton(
-//          padding: const EdgeInsets.all(0),
-//          child: const Text(
-//            '关闭',
-//          ),
-//          onPressed: () {
-//            Navigator.pop(context);
-//          },
-//        ),
         trailing: Container(
           width: 90,
           child: Row(
@@ -46,6 +63,7 @@ class _GallerySearchPageState extends State<GallerySearchPage> {
                 ),
                 onPressed: () {
                   Global.logger.v('search Btn');
+                  _jumpSearch();
                 },
               ),
               CupertinoButton(
