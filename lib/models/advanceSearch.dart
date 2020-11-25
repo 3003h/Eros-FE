4fdbc8b2e7291d092e73f8de65aa0db0b686443b
honjow <honjow311@gmail.com>
2020-11-26 03:07:49 +0800

    高级搜索

---
+++ lib/models/advanceSearch.dart
@@ -0,0 +1,29 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'advanceSearch.g.dart';
+
+@JsonSerializable()
+class AdvanceSearch {
+  AdvanceSearch();
+
+  bool searchGalleryName;
+  bool searchGalleryTags;
+  bool searchGalleryDesc;
+  bool searchToreenFilenames;
+  bool onlyShowWhithTorrents;
+  bool searchLowPowerTags;
+  bool searchDownvotedTags;
+  bool searchExpunged;
+  bool searchWithminRating;
+  int minRating;
+  bool searchBetweenpage;
+  String startPage;
+  String endPage;
+  bool disableDFLanguage;
+  bool disableDFUploader;
+  bool disableDFTags;
+
+  factory AdvanceSearch.fromJson(Map<String, dynamic> json) =>
+      _$AdvanceSearchFromJson(json);
+  Map<String, dynamic> toJson() => _$AdvanceSearchToJson(this);
+}
---
+++ lib/models/advanceSearch.g.dart
@@ -0,0 +1,47 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'advanceSearch.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+AdvanceSearch _$AdvanceSearchFromJson(Map<String, dynamic> json) {
+  return AdvanceSearch()
+    ..searchGalleryName = json['searchGalleryName'] as bool
+    ..searchGalleryTags = json['searchGalleryTags'] as bool
+    ..searchGalleryDesc = json['searchGalleryDesc'] as bool
+    ..searchToreenFilenames = json['searchToreenFilenames'] as bool
+    ..onlyShowWhithTorrents = json['onlyShowWhithTorrents'] as bool
+    ..searchLowPowerTags = json['searchLowPowerTags'] as bool
+    ..searchDownvotedTags = json['searchDownvotedTags'] as bool
+    ..searchExpunged = json['searchExpunged'] as bool
+    ..searchWithminRating = json['searchWithminRating'] as bool
+    ..minRating = json['minRating'] as int
+    ..searchBetweenpage = json['searchBetweenpage'] as bool
+    ..startPage = json['startPage'] as String
+    ..endPage = json['endPage'] as String
+    ..disableDFLanguage = json['disableDFLanguage'] as bool
+    ..disableDFUploader = json['disableDFUploader'] as bool
+    ..disableDFTags = json['disableDFTags'] as bool;
+}
+
+Map<String, dynamic> _$AdvanceSearchToJson(AdvanceSearch instance) =>
+    <String, dynamic>{
+      'searchGalleryName': instance.searchGalleryName,
+      'searchGalleryTags': instance.searchGalleryTags,
+      'searchGalleryDesc': instance.searchGalleryDesc,
+      'searchToreenFilenames': instance.searchToreenFilenames,
+      'onlyShowWhithTorrents': instance.onlyShowWhithTorrents,
+      'searchLowPowerTags': instance.searchLowPowerTags,
+      'searchDownvotedTags': instance.searchDownvotedTags,
+      'searchExpunged': instance.searchExpunged,
+      'searchWithminRating': instance.searchWithminRating,
+      'minRating': instance.minRating,
+      'searchBetweenpage': instance.searchBetweenpage,
+      'startPage': instance.startPage,
+      'endPage': instance.endPage,
+      'disableDFLanguage': instance.disableDFLanguage,
+      'disableDFUploader': instance.disableDFUploader,
+      'disableDFTags': instance.disableDFTags,
+    };
