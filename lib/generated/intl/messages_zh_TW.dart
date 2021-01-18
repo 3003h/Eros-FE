b26e3a471111150fd559267d5530ae87dfc389ab
honjow <honjow311@gmail.com>
2021-01-19 00:21:51 +0800

    add arb file

---
+++ lib/generated/intl/messages_zh_TW.dart
@@ -0,0 +1,144 @@
+// DO NOT EDIT. This is code generated via package:intl/generate_localized.dart
+// This is a library that provides messages for a zh_TW locale. All the
+// messages from the main program should be duplicated here with the same
+// function name.
+
+// Ignore issues from commonly used lints in this file.
+// ignore_for_file:unnecessary_brace_in_string_interps, unnecessary_new
+// ignore_for_file:prefer_single_quotes,comment_references, directives_ordering
+// ignore_for_file:annotate_overrides,prefer_generic_function_type_aliases
+// ignore_for_file:unused_import, file_names
+
+import 'package:intl/intl.dart';
+import 'package:intl/message_lookup_by_library.dart';
+
+final messages = new MessageLookup();
+
+typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
+
+class MessageLookup extends MessageLookupByLibrary {
+  String get localeName => 'zh_TW';
+
+  static m0(site) => "当前 ${site}";
+
+  static m1(rating) => "${rating} ⭐";
+
+  final messages = _notInlinedMessages(_notInlinedMessages);
+  static _notInlinedMessages(_) => <String, Function> {
+    "KEEP_IT_SAFE" : MessageLookupByLibrary.simpleMessage("注意数据安全"),
+    "READ" : MessageLookupByLibrary.simpleMessage("閱讀"),
+    "about" : MessageLookupByLibrary.simpleMessage("關於"),
+    "add_quick_search" : MessageLookupByLibrary.simpleMessage("添加搜索"),
+    "add_to_favorites" : MessageLookupByLibrary.simpleMessage("添加收藏"),
+    "advanced" : MessageLookupByLibrary.simpleMessage("高級"),
+    "all_Favorites" : MessageLookupByLibrary.simpleMessage("所有收藏"),
+    "all_comment" : MessageLookupByLibrary.simpleMessage("所有评论"),
+    "all_preview" : MessageLookupByLibrary.simpleMessage("所有预览"),
+    "app_title" : MessageLookupByLibrary.simpleMessage("FEhViewer"),
+    "cancel" : MessageLookupByLibrary.simpleMessage("取消"),
+    "clear_cache" : MessageLookupByLibrary.simpleMessage("清除缓存"),
+    "clear_filter" : MessageLookupByLibrary.simpleMessage("重置"),
+    "copied_to_clipboard" : MessageLookupByLibrary.simpleMessage("已复制到剪贴板"),
+    "copy" : MessageLookupByLibrary.simpleMessage("复制"),
+    "current_site" : m0,
+    "current_version" : MessageLookupByLibrary.simpleMessage("当前版本"),
+    "custom_hosts" : MessageLookupByLibrary.simpleMessage("自定义hosts"),
+    "dark" : MessageLookupByLibrary.simpleMessage("深色模式"),
+    "dark_mode_effect" : MessageLookupByLibrary.simpleMessage("深色模式效果"),
+    "delete" : MessageLookupByLibrary.simpleMessage("刪除"),
+    "domain_fronting" : MessageLookupByLibrary.simpleMessage("域名前置"),
+    "double_click_back" : MessageLookupByLibrary.simpleMessage("再按一次返回退出"),
+    "download" : MessageLookupByLibrary.simpleMessage("下載"),
+    "eh" : MessageLookupByLibrary.simpleMessage("E·H"),
+    "ehentai_my_tags" : MessageLookupByLibrary.simpleMessage("我的标签"),
+    "ehentai_settings" : MessageLookupByLibrary.simpleMessage("EHentai 设置"),
+    "favcat" : MessageLookupByLibrary.simpleMessage("收藏夾"),
+    "favorites_order" : MessageLookupByLibrary.simpleMessage("收藏夹排序"),
+    "favorites_order_Use_favorited" : MessageLookupByLibrary.simpleMessage("按照收藏时间"),
+    "favorites_order_Use_posted" : MessageLookupByLibrary.simpleMessage("按照发布时间"),
+    "follow_system" : MessageLookupByLibrary.simpleMessage("跟随系统"),
+    "galery_site" : MessageLookupByLibrary.simpleMessage("画廊站点"),
+    "gallery_comments" : MessageLookupByLibrary.simpleMessage("畫廊評論"),
+    "gray_black" : MessageLookupByLibrary.simpleMessage("灰黑"),
+    "language" : MessageLookupByLibrary.simpleMessage("语言设置"),
+    "letf_to_right" : MessageLookupByLibrary.simpleMessage("从左到右"),
+    "light" : MessageLookupByLibrary.simpleMessage("淺色模式"),
+    "list_load_more_fail" : MessageLookupByLibrary.simpleMessage("加载失败, 点击重试"),
+    "list_mode" : MessageLookupByLibrary.simpleMessage("列表浏览模式"),
+    "listmode_medium" : MessageLookupByLibrary.simpleMessage("列表 - 中"),
+    "listmode_small" : MessageLookupByLibrary.simpleMessage("列表 - 小"),
+    "listmode_waterfall" : MessageLookupByLibrary.simpleMessage("瀑布流"),
+    "local_favorite" : MessageLookupByLibrary.simpleMessage("本地收藏"),
+    "login" : MessageLookupByLibrary.simpleMessage("login"),
+    "login_web" : MessageLookupByLibrary.simpleMessage("通过网页登录"),
+    "max_history" : MessageLookupByLibrary.simpleMessage("最大历史记录"),
+    "morePreviews" : MessageLookupByLibrary.simpleMessage("更多预览"),
+    "mytags_on_website" : MessageLookupByLibrary.simpleMessage("网页上的标签设置"),
+    "noMorePreviews" : MessageLookupByLibrary.simpleMessage("无更多预览"),
+    "notFav" : MessageLookupByLibrary.simpleMessage("未收藏"),
+    "not_login" : MessageLookupByLibrary.simpleMessage("未登录"),
+    "off" : MessageLookupByLibrary.simpleMessage("關閉"),
+    "ok" : MessageLookupByLibrary.simpleMessage("確定"),
+    "on" : MessageLookupByLibrary.simpleMessage("打開"),
+    "p_Archiver" : MessageLookupByLibrary.simpleMessage("归档"),
+    "p_Download" : MessageLookupByLibrary.simpleMessage("下载"),
+    "p_Rate" : MessageLookupByLibrary.simpleMessage("评分"),
+    "p_Similar" : MessageLookupByLibrary.simpleMessage("相似"),
+    "p_Torrent" : MessageLookupByLibrary.simpleMessage("种子"),
+    "passwd" : MessageLookupByLibrary.simpleMessage("密碼"),
+    "pls_i_passwd" : MessageLookupByLibrary.simpleMessage("請輸入密碼"),
+    "pls_i_username" : MessageLookupByLibrary.simpleMessage("請輸入用戶名"),
+    "preload_image" : MessageLookupByLibrary.simpleMessage("预载图片数量"),
+    "processing" : MessageLookupByLibrary.simpleMessage("處理中"),
+    "pure_black" : MessageLookupByLibrary.simpleMessage("純黑"),
+    "quick_search" : MessageLookupByLibrary.simpleMessage("快速搜索"),
+    "read_setting" : MessageLookupByLibrary.simpleMessage("阅读设置"),
+    "reading_direction" : MessageLookupByLibrary.simpleMessage("阅读方向"),
+    "reload_image" : MessageLookupByLibrary.simpleMessage("重新载入图片"),
+    "remove_from_favorites" : MessageLookupByLibrary.simpleMessage("从收藏中删除"),
+    "right_to_left" : MessageLookupByLibrary.simpleMessage("从右到左"),
+    "s_Advanced_Options" : MessageLookupByLibrary.simpleMessage("高级选项"),
+    "s_Between" : MessageLookupByLibrary.simpleMessage("从"),
+    "s_Disable_default_filters" : MessageLookupByLibrary.simpleMessage("禁用默认排除选项"),
+    "s_Minimum_Rating" : MessageLookupByLibrary.simpleMessage("最低评分"),
+    "s_Only_Show_Galleries_With_Torrents" : MessageLookupByLibrary.simpleMessage("仅显示有种子的画廊"),
+    "s_Search_Downvoted_Tags" : MessageLookupByLibrary.simpleMessage("搜索差评标签"),
+    "s_Search_Fav_Name" : MessageLookupByLibrary.simpleMessage("搜索名称"),
+    "s_Search_Fav_Note" : MessageLookupByLibrary.simpleMessage("搜索标注"),
+    "s_Search_Fav_Tags" : MessageLookupByLibrary.simpleMessage("搜索标签"),
+    "s_Search_Gallery_Description" : MessageLookupByLibrary.simpleMessage("搜索画廊描述"),
+    "s_Search_Gallery_Name" : MessageLookupByLibrary.simpleMessage("搜索画廊名字"),
+    "s_Search_Gallery_Tags" : MessageLookupByLibrary.simpleMessage("搜索画廊标签"),
+    "s_Search_Low_Power_Tags" : MessageLookupByLibrary.simpleMessage("搜索低愿力标签"),
+    "s_Search_Torrent_Filenames" : MessageLookupByLibrary.simpleMessage("搜索种子名字"),
+    "s_Show_Expunged_Galleries" : MessageLookupByLibrary.simpleMessage("搜索被删除的画廊"),
+    "s_and" : MessageLookupByLibrary.simpleMessage("到"),
+    "s_pages" : MessageLookupByLibrary.simpleMessage("页数"),
+    "s_stars" : m1,
+    "search" : MessageLookupByLibrary.simpleMessage("搜索"),
+    "search_type" : MessageLookupByLibrary.simpleMessage("搜索类型"),
+    "setting_on_website" : MessageLookupByLibrary.simpleMessage("网页上的设置"),
+    "share" : MessageLookupByLibrary.simpleMessage("分享"),
+    "share_image" : MessageLookupByLibrary.simpleMessage("分享图片"),
+    "show_filter" : MessageLookupByLibrary.simpleMessage("显示筛选"),
+    "show_jpn_title" : MessageLookupByLibrary.simpleMessage("显示日文标题"),
+    "t_Clear_all_history" : MessageLookupByLibrary.simpleMessage("清除所有历史"),
+    "tab_download" : MessageLookupByLibrary.simpleMessage("下載"),
+    "tab_favorite" : MessageLookupByLibrary.simpleMessage("收藏"),
+    "tab_gallery" : MessageLookupByLibrary.simpleMessage("畫廊"),
+    "tab_history" : MessageLookupByLibrary.simpleMessage("歷史"),
+    "tab_popular" : MessageLookupByLibrary.simpleMessage("熱門"),
+    "tab_setting" : MessageLookupByLibrary.simpleMessage("設置"),
+    "tab_sort" : MessageLookupByLibrary.simpleMessage("长按并拖动以进行排序"),
+    "tab_watched" : MessageLookupByLibrary.simpleMessage("關注"),
+    "tabbar_setting" : MessageLookupByLibrary.simpleMessage("主页标签设置"),
+    "tags" : MessageLookupByLibrary.simpleMessage("标签"),
+    "theme" : MessageLookupByLibrary.simpleMessage("主題"),
+    "top_to_bottom" : MessageLookupByLibrary.simpleMessage("从上到下"),
+    "unlimited" : MessageLookupByLibrary.simpleMessage("不限制"),
+    "uploader" : MessageLookupByLibrary.simpleMessage("上传者"),
+    "user_login" : MessageLookupByLibrary.simpleMessage("用戶登陸"),
+    "user_name" : MessageLookupByLibrary.simpleMessage("用戶名"),
+    "welcome_text" : MessageLookupByLibrary.simpleMessage("小撸怡情~大撸伤身~强撸灰飞烟灭~~")
+  };
+}
