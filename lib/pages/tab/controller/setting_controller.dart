6328adb520bc8256b761970a81427d258a6a7b9b
honjow <honjow311@gmail.com>
2021-01-22 14:37:45 +0800

    设置布局调整

--- lib/pages/tab/controller/setting_controller.dart
+++ lib/pages/tab/controller/setting_controller.dart
@@ -1,11 +1,56 @@
 import 'package:fehviewer/generated/l10n.dart';
 import 'package:fehviewer/pages/item/setting_item.dart';
+import 'package:fehviewer/pages/tab/controller/tabhome_controller.dart';
 import 'package:fehviewer/route/routes.dart';
 import 'package:flutter/widgets.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:get/get.dart';
 
-class SettingViewController extends GetxController {
+class SettingViewController extends GetxController
+    with SingleGetTickerProviderMixin {
+  // 控制middle显示
+  final RxBool _showMiddle = false.obs;
+  bool get showMidle => _showMiddle.value;
+  set showMidle(bool val) => _showMiddle.value = val;
+
+  //
+  Animation<double> animation;
+  AnimationController _animationController;
+
+  final ScrollController _scrollController =
+      tabPages.scrollControllerMap[EHRoutes.setting];
+
+  @override
+  void onInit() {
+    super.onInit();
+
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 150),
+    );
+
+    animation = Tween(begin: 0.0, end: 1.0).animate(_animationController);
+
+    // 监听滚动
+    _scrollController.addListener(_scrollControllerLister);
+  }
+
+  void _scrollControllerLister() {
+    if (_scrollController.offset < 50.0 && showMidle) {
+      _animationController.reverse();
+      showMidle = false;
+    } else if (_scrollController.offset >= 50.0 && !showMidle) {
+      _animationController.forward();
+      showMidle = true;
+    }
+  }
+
+  @override
+  void onClose() {
+    _animationController.dispose();
+    super.onClose();
+  }
+
   // 菜单文案
   var _itemTitles = [];
 
