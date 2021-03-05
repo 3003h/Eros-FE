3c61bf2d2e296acff074d1d3c63a335153cb6b9d
honjow <honjow311@gmail.com>
2021-03-05 21:15:13 +0800

    更新Podfile

--- lib/utils/dns_util.dart
+++ lib/utils/dns_util.dart
@@ -1,22 +1,10 @@
-import 'dart:io';
-
-import 'package:dns_client/dns_client.dart';
-import 'package:fehviewer/utils/logger.dart';
-import 'package:fehviewer/utils/utility.dart';
+import 'package:basic_utils/basic_utils.dart';
 
 class DnsUtil {
   static Future<String> doh(String host,
-      {DohResolve dhoResolve = DohResolve.cloudflare}) async {
-    logger.d(' DnsUtil.doh $host');
-    final DnsOverHttps dns = dhoResolve == DohResolve.cloudflare
-        ? DnsOverHttps.cloudflare()
-        : DnsOverHttps.google();
-    final List<InternetAddress> response = await dns.lookup(host.trim());
-    // logger.d('$response');
-    if (response.isNotEmpty) {
-      return (response..shuffle()).first.address;
-    } else {
-      return host;
-    }
+      {DnsApiProvider dhoResolve = DnsApiProvider.CLOUDFLARE}) async {
+    final List<RRecord> response =
+        await DnsUtils.reverseDns(host, provider: dhoResolve);
+    return (response..shuffle()).first.data;
   }
 }
