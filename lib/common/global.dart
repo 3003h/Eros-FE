d6bf4012317ea8961a34937cb60208de0e6c165c
honjow <honjow311@gmail.com>
2021-05-18 20:04:05 +0800

    初步的下载功能

--- lib/common/global.dart
+++ lib/common/global.dart
@@ -164,6 +164,7 @@ class Global {
   static String appSupportPath = '';
   static String appDocPath = '';
   static String tempPath = '';
+  static late String extStorePath;
 
   static late PackageInfo packageInfo;
 
@@ -197,6 +198,9 @@ class Global {
     appSupportPath = (await getApplicationSupportDirectory()).path;
     appDocPath = (await getApplicationDocumentsDirectory()).path;
     tempPath = (await getTemporaryDirectory()).path;
+    extStorePath = !Platform.isIOS
+        ? (await getExternalStorageDirectory())?.path ?? ''
+        : '';
 
     packageInfo = await PackageInfo.fromPlatform();
 
