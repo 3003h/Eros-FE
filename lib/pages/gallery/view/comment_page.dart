7f321140b359a0feb13cb529b76ac43da21a2886
honjow <honjow311@gmail.com>
2021-06-14 00:06:00 +0800

    Fix: 修复发送评论时可能会出现的异常问题

--- lib/pages/gallery/view/comment_page.dart
+++ lib/pages/gallery/view/comment_page.dart
@@ -7,6 +7,7 @@ import 'package:fehviewer/models/base/eh_models.dart';
 import 'package:fehviewer/pages/gallery/controller/comment_controller.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:get/get.dart';
 
@@ -69,9 +70,11 @@ class CommentPage extends StatelessWidget {
                           controller.oriComment,
                           maxLines: 3,
                           overflow: TextOverflow.ellipsis,
-                          style: const TextStyle(
+                          style: TextStyle(
                             fontWeight: FontWeight.w500,
                             fontSize: 16,
+                            color: CupertinoDynamicColor.resolve(
+                                CupertinoColors.label, context),
                           ),
                         ),
                       ],
@@ -98,112 +101,115 @@ class CommentPage extends StatelessWidget {
       );
     }
 
-    return CupertinoPageScaffold(
-      // resizeToAvoidBottomInset: false,
-      navigationBar: CupertinoNavigationBar(
-        middle: Text(S.of(context).gallery_comments),
-        previousPageTitle: S.of(context).back,
-      ),
-      // child: commList,
-      child: SafeArea(
-        // minimum: const EdgeInsets.only(bottom: 20),
-        bottom: false,
-        child: Stack(
-          alignment: Alignment.bottomCenter,
-          children: <Widget>[
-            GestureDetector(
-              // behavior: HitTestBehavior.deferToChild,
-              onTap: () {
-                // 触摸收起键盘
-                FocusScope.of(context).requestFocus(FocusNode());
-              },
-              onPanDown: (DragDownDetails details) {
-                // 滑动收起键盘
-                // FocusScope.of(context).requestFocus(FocusNode());
-              },
-              child: commList,
-            ),
-            SingleChildScrollView(
-              child: Container(
-                padding:
-                    EdgeInsets.only(bottom: context.mediaQueryPadding.bottom),
-                decoration: BoxDecoration(
-                  border: _kDefaultEditBorder,
-                  color: CupertinoDynamicColor.resolve(
-                          ehTheme.themeData!.barBackgroundColor, context)
-                      .withOpacity(1),
-                  // color: Colors.transparent,
-                ),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    // 编辑原消息时的显示
-                    _buildOriText(),
-                    // 输入框
-                    Row(
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      children: <Widget>[
-                        Expanded(
-                          child: Padding(
-                            padding: const EdgeInsets.fromLTRB(8, 8, 0, 8),
-                            child: CupertinoTextField(
+    return FlutterEasyLoading(
+      child: CupertinoPageScaffold(
+        // resizeToAvoidBottomInset: false,
+        navigationBar: CupertinoNavigationBar(
+          middle: Text(S.of(context).gallery_comments),
+          previousPageTitle: S.of(context).back,
+        ),
+        // child: commList,
+        child: SafeArea(
+          // minimum: const EdgeInsets.only(bottom: 20),
+          bottom: false,
+          child: Stack(
+            alignment: Alignment.bottomCenter,
+            children: <Widget>[
+              GestureDetector(
+                // behavior: HitTestBehavior.deferToChild,
+                onTap: () {
+                  // 触摸收起键盘
+                  FocusScope.of(context).requestFocus(FocusNode());
+                },
+                onPanDown: (DragDownDetails details) {
+                  // 滑动收起键盘
+                  // FocusScope.of(context).requestFocus(FocusNode());
+                },
+                child: commList,
+              ),
+              SingleChildScrollView(
+                child: Container(
+                  padding:
+                      EdgeInsets.only(bottom: context.mediaQueryPadding.bottom),
+                  decoration: BoxDecoration(
+                    border: _kDefaultEditBorder,
+                    color: CupertinoDynamicColor.resolve(
+                            ehTheme.themeData!.barBackgroundColor, context)
+                        .withOpacity(1),
+                    // color: Colors.transparent,
+                  ),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      // 编辑原消息时的显示
+                      _buildOriText(),
+                      // 输入框
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: <Widget>[
+                          Expanded(
+                            child: Padding(
+                              padding: const EdgeInsets.fromLTRB(8, 8, 0, 8),
+                              child: CupertinoTextField(
+                                padding: const EdgeInsets.symmetric(
+                                    horizontal: 14.0, vertical: 6.0),
+                                maxLines: null,
+                                keyboardType: TextInputType.multiline,
+                                decoration: BoxDecoration(
+                                  color:
+                                      ehTheme.commentTextFieldBackgroundColor,
+                                  borderRadius: const BorderRadius.all(
+                                      Radius.circular(18.0)),
+                                ),
+                                controller: controller.commentTextController,
+                                focusNode: controller.focusNode,
+                                placeholder: 'New comment',
+                              ),
+                            ),
+                          ),
+                          CupertinoTheme(
+                            data: const CupertinoThemeData(
+                              primaryColor: CupertinoColors.activeGreen,
+                            ),
+                            child: CupertinoButton(
                               padding: const EdgeInsets.symmetric(
-                                  horizontal: 14.0, vertical: 6.0),
-                              maxLines: null,
-                              keyboardType: TextInputType.multiline,
-                              decoration: BoxDecoration(
-                                color: ehTheme.commentTextFieldBackgroundColor,
-                                borderRadius: const BorderRadius.all(
-                                    Radius.circular(18.0)),
+                                  horizontal: 6, vertical: 8),
+                              minSize: 0,
+                              child: Obx(
+                                () => AnimatedSwitcher(
+                                  duration: const Duration(milliseconds: 200),
+                                  switchInCurve: Curves.bounceIn,
+                                  // switchOutCurve: Curves.linear,
+                                  transitionBuilder: (child, animation) =>
+                                      // ScaleTransition(
+                                      //     scale: animation, child: child),
+                                      FadeTransition(
+                                          child: child, opacity: animation),
+                                  child: controller.isEditStat
+                                      ? Icon(
+                                          FontAwesomeIcons.solidCheckCircle,
+                                          key: UniqueKey(),
+                                          size: 32,
+                                        )
+                                      : Icon(
+                                          FontAwesomeIcons.arrowCircleUp,
+                                          key: UniqueKey(),
+                                          size: 32,
+                                        ),
+                                ),
                               ),
-                              controller: controller.commentTextController,
-                              focusNode: controller.focusNode,
-                              placeholder: 'New comment',
+                              onPressed: controller.pressSend,
                             ),
                           ),
-                        ),
-                        CupertinoTheme(
-                          data: const CupertinoThemeData(
-                            primaryColor: CupertinoColors.activeGreen,
-                          ),
-                          child: CupertinoButton(
-                            padding: const EdgeInsets.symmetric(
-                                horizontal: 6, vertical: 8),
-                            minSize: 0,
-                            child: Obx(
-                              () => AnimatedSwitcher(
-                                duration: const Duration(milliseconds: 200),
-                                switchInCurve: Curves.bounceIn,
-                                // switchOutCurve: Curves.linear,
-                                transitionBuilder: (child, animation) =>
-                                    // ScaleTransition(
-                                    //     scale: animation, child: child),
-                                    FadeTransition(
-                                        child: child, opacity: animation),
-                                child: controller.isEditStat
-                                    ? Icon(
-                                        FontAwesomeIcons.solidCheckCircle,
-                                        key: UniqueKey(),
-                                        size: 32,
-                                      )
-                                    : Icon(
-                                        FontAwesomeIcons.arrowCircleUp,
-                                        key: UniqueKey(),
-                                        size: 32,
-                                      ),
-                              ),
-                            ),
-                            onPressed: controller.pressSend,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
