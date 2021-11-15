8cc4c679c6c504a9147b89785a34a1eee3882def
honjow <honjow311@gmail.com>
2021-11-15 19:24:58 +0800

    Fix: 修复预览页跳转

--- lib/pages/gallery/view/all_preview_page.dart
+++ lib/pages/gallery/view/all_preview_page.dart
@@ -47,97 +47,88 @@ class _AllPreviewPageState extends State<AllPreviewPage> {
         controller: controller.scrollController,
         child: controller.obx((List<GalleryImage>? state) {
           if (state != null) {
-            return SizeCacheWidget(
-              child: CustomScrollView(
-                controller: controller.scrollController,
-                slivers: <Widget>[
-                  SliverSafeArea(
-                    sliver: SliverPadding(
-                      padding: const EdgeInsets.only(top: 4, left: 8, right: 8),
-                      sliver: SliverGrid(
-                        gridDelegate:
-                            const SliverGridDelegateWithMaxCrossAxisExtent(
-                                maxCrossAxisExtent: kMaxCrossAxisExtent,
-                                mainAxisSpacing: kMainAxisSpacing, //主轴方向的间距
-                                crossAxisSpacing:
-                                    kCrossAxisSpacing, //交叉轴方向子元素的间距
-                                childAspectRatio: kChildAspectRatio //显示区域宽高比
-                                ),
-                        delegate: SliverChildBuilderDelegate(
-                          (BuildContext context, int index) {
-                            //如果显示到最后一个 获取下一页缩略图
-                            if ((index == state.length - 1 ||
-                                    state[index + 1].ser - state[index].ser >
-                                        1) &&
-                                index < _count - 1) {
-                              controller.fetchPriviews();
-                            } else if (index >= _count - 1) {
-                              controller.fetchFinsh();
-                            }
-                            return FrameSeparateWidget(
-                              index: index,
-                              child: Center(
-                                key: index == 0 ? controller.globalKey : null,
-                                child: PreviewContainer(
-                                  galleryImageList: state,
-                                  index: index,
-                                  gid: controller.gid,
-                                  onLoadComplet: () {
-                                    final thumbUrl =
-                                        state[index].thumbUrl ?? '';
-                                    Future.delayed(
-                                            const Duration(milliseconds: 50))
-                                        .then(
-                                      (_) {
-                                        if (!(_loadComplets[thumbUrl] ??
-                                                false) &&
-                                            mounted) {
-                                          logger.d('onLoadComplet $thumbUrl');
-                                          setState(
-                                            () {
-                                              _loadComplets[thumbUrl] = true;
-                                            },
-                                          );
-                                        }
-                                      },
-                                    );
-                                  },
-                                ),
+            return CustomScrollView(
+              controller: controller.scrollController,
+              slivers: <Widget>[
+                SliverSafeArea(
+                  sliver: SliverPadding(
+                    padding: const EdgeInsets.only(top: 4, left: 8, right: 8),
+                    sliver: SliverGrid(
+                      gridDelegate:
+                          const SliverGridDelegateWithMaxCrossAxisExtent(
+                              maxCrossAxisExtent: kMaxCrossAxisExtent,
+                              mainAxisSpacing: kMainAxisSpacing, //主轴方向的间距
+                              crossAxisSpacing: kCrossAxisSpacing, //交叉轴方向子元素的间距
+                              childAspectRatio: kChildAspectRatio //显示区域宽高比
                               ),
-                            );
-                          },
-                          childCount: state.length,
-                        ),
+                      delegate: SliverChildBuilderDelegate(
+                        (BuildContext context, int index) {
+                          //如果显示到最后一个 获取下一页缩略图
+                          if ((index == state.length - 1 ||
+                                  state[index + 1].ser - state[index].ser >
+                                      1) &&
+                              index < _count - 1) {
+                            controller.fetchPriviews();
+                          } else if (index >= _count - 1) {
+                            controller.fetchFinsh();
+                          }
+                          return Center(
+                            key: index == 0 ? controller.globalKey : null,
+                            child: PreviewContainer(
+                              galleryImageList: state,
+                              index: index,
+                              gid: controller.gid,
+                              onLoadComplet: () {
+                                final thumbUrl = state[index].thumbUrl ?? '';
+                                Future.delayed(const Duration(milliseconds: 50))
+                                    .then(
+                                  (_) {
+                                    if (!(_loadComplets[thumbUrl] ?? false) &&
+                                        mounted) {
+                                      logger.d('onLoadComplet $thumbUrl');
+                                      setState(
+                                        () {
+                                          _loadComplets[thumbUrl] = true;
+                                        },
+                                      );
+                                    }
+                                  },
+                                );
+                              },
+                            ),
+                          );
+                        },
+                        childCount: state.length,
                       ),
                     ),
                   ),
-                  SliverToBoxAdapter(
-                    child: Container(
-                      padding: const EdgeInsets.only(bottom: 50, top: 10),
-                      child: Column(
-                        children: <Widget>[
-                          if (controller.isLoading)
-                            const CupertinoActivityIndicator(
-                              radius: 14,
-                            )
-                          else
-                            Container(),
-                          if (controller.isLoadFinsh)
-                            Container(
-                              padding: const EdgeInsets.only(top: 0),
-                              child: Text(
-                                L10n.of(context).noMorePreviews,
-                                style: const TextStyle(fontSize: 14),
-                              ),
-                            )
-                          else
-                            Container(),
-                        ],
-                      ),
+                ),
+                SliverToBoxAdapter(
+                  child: Container(
+                    padding: const EdgeInsets.only(bottom: 50, top: 10),
+                    child: Column(
+                      children: <Widget>[
+                        if (controller.isLoading)
+                          const CupertinoActivityIndicator(
+                            radius: 14,
+                          )
+                        else
+                          Container(),
+                        if (controller.isLoadFinsh)
+                          Container(
+                            padding: const EdgeInsets.only(top: 0),
+                            child: Text(
+                              L10n.of(context).noMorePreviews,
+                              style: const TextStyle(fontSize: 14),
+                            ),
+                          )
+                        else
+                          Container(),
+                      ],
                     ),
-                  )
-                ],
-              ),
+                  ),
+                )
+              ],
             );
           } else {
             return const SizedBox.shrink();
