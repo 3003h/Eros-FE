a03aab384fab4dade6b3206816b63733a9d22a4b
honjow <honjow311@gmail.com>
2021-12-31 00:51:37 +0800

    Fix: 优化取值

--- lib/utils/sentry.dart
+++ lib/utils/sentry.dart
@@ -3,7 +3,9 @@ import 'dart:convert';
 import 'package:flutter/services.dart';
 
 Future<String?> getSentryDsn() async {
-  final String sentry = await rootBundle.loadString('assets/sentry.json');
-  final jsonObj = json.decode(sentry);
-  return jsonObj['dsn'] as String?;
+  try {
+    final String sentry = await rootBundle.loadString('assets/sentry.json');
+    final jsonObj = json.decode(sentry);
+    return jsonObj['dsn'] as String?;
+  } catch (_) {}
 }
