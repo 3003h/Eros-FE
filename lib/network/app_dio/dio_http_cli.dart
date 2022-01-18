fee6e37842bae4b66352758a1570beef4569f994
honjow <honjow311@gmail.com>
2022-01-18 15:31:40 +0800

    Feat：可恢复默认下载路径。主题问题修正

--- lib/network/app_dio/dio_http_cli.dart
+++ lib/network/app_dio/dio_http_cli.dart
@@ -27,7 +27,7 @@ class DioHttpClient {
       );
       return handleResponse(response, httpTransformer: httpTransformer);
     } on DioError catch (e, stack) {
-      logger.e('DioError:\n$e\n$stack');
+      // logger.e('DioError:\n$e\n$stack');
       return handleException(e, data: e.response?.data);
     } on Exception catch (e, stack) {
       logger.e('Exception:\n$e\n$stack');
