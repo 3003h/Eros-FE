26f62b42b64e448ca6b189582bbf205f1ec529d8
honjow <honjow311@gmail.com>
2022-03-31 01:54:36 +0800

    Feat: build windows

--- win.bat
+++ win.bat
@@ -1,18 +1,2 @@
 @echo off
-
-flutter build windows --release
-
-set DestDir=build\windows\runner\fehviewer
-set SrcDir=build\windows\runner\Release
-
-if exist %DestDir%\ (
-    echo %DestDir% exist
-    del /f /q %DestDir%
-) else (
-    echo mkdir %DestDir%
-    md %DestDir%
-)
-::rd /s /q %DestDir%
-
-xcopy /e /h /q /y %SrcDir% %DestDir%
-xcopy /y windows\*.dll %SrcDir%\*
+flutter build windows --release && win_comp.bat
---
+++ win_comp.bat
@@ -0,0 +1,16 @@
+@echo off
+
+set DestDir=build\windows\runner\fehviewer
+set SrcDir=build\windows\runner\Release
+
+if exist %DestDir%\ (
+    echo %DestDir% exist
+    del /f /q %DestDir%
+) else (
+    echo mkdir %DestDir%
+    md %DestDir%
+)
+::rd /s /q %DestDir%
+
+xcopy /e /h /q /y %SrcDir% %DestDir%
+xcopy /y windows\*.dll %SrcDir%\*
