97791814a86d877f24fd1386e250a57d95e0e797
honjow <honjow311@gmail.com>
2022-05-26 18:45:44 +0800

    修正sample文件

--- lib/common/service/ehconfig_service.dart
+++ lib/common/service/ehconfig_service.dart
@@ -589,7 +589,7 @@ class EhConfigService extends ProfileService {
 
     final String _text =
         (await Clipboard.getData(Clipboard.kTextPlain))?.text ?? '';
-    logger.d('Clipboard ' + _text);
+    logger.d('Clipboard: ' + _text);
     final RegExp _reg =
         RegExp(r'https?://e[-|x]hentai.org/g/\d+/[0-9a-f]{10}/?');
     final RegExpMatch? _mach = _reg.firstMatch(_text);
