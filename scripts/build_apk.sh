b70c1dec664c8dec18b23934c10f66143062d3b6
honjow <honjow311@gmail.com>
2022-05-26 14:39:15 +0800

    build scripts

--- scripts/build_apk.sh
+++ scripts/build_apk.sh
@@ -6,7 +6,7 @@ echo $apk_build_path_universal
 
 
 #flutter build apk --target-platform android-arm,android-arm64,android-x64 --split-per-abi
-flutter build apk
+flutter build apk -t lib/main.firebase.dart
 cp -R $apk_build_path $apk_build_path_universal
-flutter build apk --split-per-abi
+flutter build apk --split-per-abi -t lib/main.firebase.dart
 cp $apk_build_path_universal/*.apk $apk_build_path/*
