a3726259558e9f260ab9fc97915177f529b40d45
honjow <honjow311@gmail.com>
2022-06-01 19:39:43 +0800

    Feat: 调整一点布局

--- scripts/build_apk.sh
+++ scripts/build_apk.sh
@@ -6,7 +6,7 @@ echo $apk_build_path_universal
 
 
 #flutter build apk --target-platform android-arm,android-arm64,android-x64 --split-per-abi
-flutter build apk -t lib/main.firebase.dart
+flutter build apk
 cp -R $apk_build_path $apk_build_path_universal
-flutter build apk --split-per-abi -t lib/main.firebase.dart
+flutter build apk --split-per-abi
 cp $apk_build_path_universal/*.apk $apk_build_path/*
