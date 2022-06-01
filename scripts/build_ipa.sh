f6fa5cae0cc1c3c5b22a9e55c75e841ddbe9c617
honjow <honjow311@gmail.com>
2022-06-01 19:39:43 +0800

    Feat: 调整一点布局

--- scripts/build_ipa.sh
+++ scripts/build_ipa.sh
@@ -1,3 +1,3 @@
 source ./para.sh
 cd $scripts_path/../ios && pod update && pod install && cd $scripts_path
-flutter build ipa --release -t lib/main.firebase.dart && sh reid.sh && sh zip.sh $version && sh dsym.sh
+flutter build ipa --release && sh reid.sh && sh zip.sh $version && sh dsym.sh
