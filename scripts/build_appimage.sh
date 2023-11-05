06ed02cfd0c218b61adec98287ea0f90c469df49
Ruben Cano Diaz <ruben.cano96@gmail.com>
2023-11-05 17:16:54 +0100

    Fix desktop, gitignore and don't redownload appimage builder

--- scripts/build_appimage.sh
+++ scripts/build_appimage.sh
@@ -4,5 +4,8 @@ mkdir -p FEhViewer.AppDir/syslib
 cp -rf ../build/linux/x64/release/bundle/* FEhViewer.AppDir/
 ldd FEhViewer.AppDir/fehviewer | cut -d' ' -f3  | xargs -I {} cp {} FEhViewer.AppDir/syslib
 unset SOURCE_DATE_EPOCH
-wget https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-x86_64.AppImage && chmod a+x *.AppImage
+
+if [ ! -f appimagetool-x86_64.AppImage ]; then
+    wget https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-x86_64.AppImage && chmod a+x *.AppImage
+fi
 ./appimagetool-x86_64.AppImage FEhViewer.AppDir/
