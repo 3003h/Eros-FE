16a2e440cea9ab96f6d6a9b69b136bbbe9a482b2
3003h <h3003n@outlook.com>
2024-10-30 09:19:51 +0800

    Revert firebase and google_mlkit version

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -2,7 +2,7 @@ import UIKit
 import Flutter
 import flutter_downloader
 
-@UIApplicationMain
+@main
 @objc class AppDelegate: FlutterAppDelegate {
     override func application(
         _ application: UIApplication,
